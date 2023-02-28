a16ec7522732c1a9f3569c2ef56dc20ce953bd6b
YinMyatWai <ymyatwai@gmail.com>
2023-02-28 10:15:58 +0630

    spring core base

---
+++ sample-di/src/main/java/com/demo/AppConfig.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@ComponentScan
+@Configuration
+public class AppConfig {
+}
